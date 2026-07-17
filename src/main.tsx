4cd55c59cfe58553a54da00f0cc2049e1df0cf6f
Chai1Chai <aasergienko25102005@gmail.com>
2026-07-17 20:13:13 +0700

    add loadpage

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+window.onload = () => {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.style.opacity = '0';
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 500); 
+  }
+};
